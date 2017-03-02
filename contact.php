614a1937b500f2fb702a12c109c7b50bf95440f0
AlexandreGuillemin <alexandre.guillemin@edu.ecetech.fr>
2017-03-02 10:20:26 +0100

    page contact

---
+++ contact.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<style>
+  .snowright{
+    float: right;
+    width: 550px;
+    height: 235px;
+
+  }
+</style>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<head>
+  <title>Maison des Ligues</title>
+</head>
+<body>
+
+ <div class="container">
+
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="Site.php">M2L</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="Les ligues.php">Les ligues</a></li>
+              <li><a href="Emploi.php">Emploi</a></li>
+              <li><a href="http://rencontre.michaelelbaz.com/">Rencontre</a></li>
+                </ul>
+              </li>
+              <ul class="nav navbar-nav-nav navbar-right">
+               <li><a href="Admin.php">Se connecter</a></li>
+               <li><a href="">Contact</a></li>
+            </ul>
+      </nav>
+      </div>
+      
+        <article class="left">
+      <form  method="post" action="formpost.php">
+<div style="padding: 5px;
+ position:fixed; 
+ border: 2px solid black;
+ right:350px;
+ left: 10px;
+  margin-left:auto; margin-right:auto; width: 800px; height: 410px">
+  <img class="snowright" src="ski.jpg">
+<h3>Contact</h3></br>
+  <p align="left">Nom</br>
+  <input type="text" name="auteur" /> 
+  
+    <p align="left">Prénom</br>
+  <input type="text" name="auteur" />
+    <p align="left">E-mail</br>
+  <input type="text" name="auteur" />  
+  </br><p align="left">Votre message</br>
+  <textarea name="message" rows="6" cols="123"></textarea></br>
+  <button type="submit" class="btn btn-success">Envoyer</button>
+</div>
+</article>
+<iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d363.14031846754244!2d6.214235122605976!3d48.697632205357934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x4794983386479251%3A0xc58bc09976474b86!2s13+Rue+Jean+Moulin%2C+54510+Tomblaine!3m2!1d48.6977444!2d6.2141532!5e0!3m2!1sfr!2sfr!4v1487927361929" width="800" height="450" frameborder="0" style="padding: 5px;
+ position:fixed; 
+ border: 2px solid black;
+ right:350px;
+ left: 10px;
+  margin-left:auto; margin-right:auto; width: 800px; height: 500px;top:550px; " allowfullscreen></iframe>
+
+  <section class="right">
+
+<div style="padding: 6px;
+  position:fixed;
+  top:100px;
+  bottom:50px;
+  left:815px;
+  right:10px;
+  border: 2px solid black; width: 325px; height: 950px;margin-left : auto;margin-right : auto;">
+         <h2>
+            COORDONNEES
+         </h2>
+         <h4>Maison des Ligues de Lorraine</h4>
+         <p>
+            <br />
+            13 rue Jean Moulin - BP 70001<br />
+            54510 TOMBLAINE<br />
+            Tél. : 03.83.18.87.02<br />
+            Fax : 03.83.18.87.03<br />
+            Email : <a href="mailto:contact@m2l.fr">contact@m2l.fr</a><br /><br />
+            <br />
+         </p>
+         <h4>Directeur</h4>
+         <p>
+            </br />
+            <span class="name">Lucien SAPIN</span><br />
+            Tél. : 03 83 18 87 02<br />
+            Fax : 03 83 18 87 03<br />
+            Email : <a href="mailto:lucien.gastaldello@m2l.fr">lucien.gastaldello@m2l.fr</a><br /><br />
+            <br />
+         </p>
+         <h4>Chargée Impact Emploi</h4>
+         <p>
+            <br />
+            <span class="name">Nathalie GENAIS</span><br />
+            Tél. : 03 83 18 87 05<br />
+            Email : <a href="mailto:martine.genoux@m2l.fr">martine.genoux@m2l.fr</a><br /><br />
+            <br />
+         </p>
+         <h4>Chargée d’infographie</h4>
+         <p>
+            <br />
+            <span class="name">Lorette GIROUX</span><br />
+            Tél. : 03 83 18 87 06<br />
+            Email : <a href="mailto:lorette.bossart@m2l.fr">lorette.bossart@m2l.fr</a><br /><br />
+            <br />
+         </p>
+         <h4>Secrétaire Comptable</h4>
+         <p>
+            <br />
+            <span class="name">Martine DUPONT</span><br />
+            Tél. : 03 83 18 87 07<br />
+            Fax : 03 83 18 87 03<br />
+            Email : <a href="mailto:jeannine.vuillemard@m2l.fr">jeannine.vuillemard@m2l.fr</a>
+          
+         </p>
+</div>
+</section>
+  </p>
+</form>
+</body>
+      </html>
