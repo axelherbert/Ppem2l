eef349b47a41a779b2ba58d2185f438d24af7d93
Axel Herbert <aherbert97@gmail.com>
2017-03-02 11:27:20 +0100

    modif mdp

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ function connectToSQL()
 {
 	try
 	{
-		$bdd = new PDO('mysql:host=localhost;dbname=connect;charset=utf8', 'root', 'root');
+		$bdd = new PDO('mysql:host=localhost;dbname=connect;charset=utf8', 'root', 'axel');
 
 		echo '<p style="color:#01DF01;">Connecté à la base de donnée SQL.</p>';
 	}
