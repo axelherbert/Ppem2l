df059b87b127690c7f5ce63d7b622e91b54e66e4
Axel Herbert <aherbert97@gmail.com>
2017-03-03 20:17:51 +0100

    Revert "Revert "modif complet""
    This reverts commit 4b190c5a6cf041266551ad49e837e311bc201b85.

--- assets/inc/booking.php
+++ assets/inc/booking.php
@@ -1,8 +1,9 @@
 <html>
+<?php ini_set('display_errors', true); ?>
 <!-- BOOTSRAP -->
-<script src="js/jquery-3.1.1.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="../js/jquery-3.1.1.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="../css/bootstrap.css">
 <head>
 </head>
 <body>
