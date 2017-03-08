deb61b9a6f8c1a0c6fb0ee2b0726759aa803a0ea
mchretien <mathieuchretien9@gmail.com>
2017-03-08 17:51:28 +0100

    modif index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php ini_set('display_errors', true); ?>
 <link rel="stylesheet" href="assets/css/bootstrap.css">
 <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
+<link rel="stylesheet" href="assets/css/home.css"/>
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.js"></script>
 <head>
@@ -11,5 +12,18 @@
 <body>
 <?php $page="Accueil"; ?>
 <?php include "assets/inc/navbar.php"; ?>
+
+<div class="container">
+      <div id="presentation">
+        <div class="wrap">
+          <section>
+          <h1>Présentation de la maison des ligues de Lorraine</h1>
+          <p>
+            <br />La Maison des Ligues de Lorraine (M2L) a pour mission de fournir des espaces et des services aux différentes ligues sportives régionales de Lorraine et à d’autres structures hébergées.
+						<br /> La M2L est une structure financée par le Conseil Régional de Lorraine dont l'administration est déléguée au Comité Régional Olympique et Sportif de Lorraine (CROSL).
+						<br /> Installée depuis 2003 dans la banlieue Nancéienne, la M2L accueille l'ensemble du mouvement sportif Lorrain qui représente près de 6 500 clubs, plus de 525 000 licenciés et près de 50 000 bénévoles.
+        		<br />
+          </p>
+        </section>
 </body>
 </html>
