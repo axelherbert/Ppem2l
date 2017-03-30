1420ee458537d334fe06226b5d76df571423c657
Axel Herbert <aherbert97@gmail.com>
2017-03-30 09:23:30 +0200

    modif news.php

--- news.php
+++ news.php
@@ -3,44 +3,68 @@
 <head>
 	<title>News</title>
 	 <title>jquery.rss example</title>
-	 <link rel="stylesheet" href="assets/css/bootstrapp.css">
-	 <link rel="stylesheet" href="assets/css/home.css">
+	 <link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css">
 	<script src="assets/js/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
 
     <script src="assets/js/rss.js"></script>
+    
     <script>
       jQuery(function($) {
-        $("#rss-feeds").rss("http://www.lequipe.fr/rss/actu_rss_Football.xml")
+        $("#rss-foot").rss("http://www.lequipe.fr/rss/actu_rss_Football.xml")
+      })
+    </script>
+      <script>
+      jQuery(function($) {
+        $("#rss-tennis").rss("http://www.lequipe.fr/rss/actu_rss_Tennis.xml")
+      })
+    </script>
+     <script>
+      jQuery(function($) {
+        $("#rss-rugby").rss("http://www.lequipe.fr/rss/actu_rss_Rugby.xml")
+      })
+    </script>
+    <script>
+      jQuery(function($) {
+        $("#rss-basket").rss("http://www.lequipe.fr/rss/actu_rss_Basket.xml")
+      })
+    </script>
+    <script>
+      jQuery(function($) {
+        $("#rss-hand").rss("http://www.lequipe.fr/rss/actu_rss_Hand.xml")
       })
     </script>
 
 </head>
 <body>
+	
 
-
-	<?php include "assets/inc/navbar.php";?>
+	<?php $page="Accueil"; ?>
+	<?php include "assets/inc/navbar.php"; ?>
 
 	<div class="jumbotron jumbotron-sm">
     <div class="container">
       <div class="row">
-        <div class="col-sm-8 col-lg-8">
+        <div class="col-sm-12 col-lg-12">
           <h2 class="h2">News</h2>
         </div>
-				<div class="col-sm-4 col-lg-4">
-					<center><img src="assets/images/logo-m2l.png" alt="Logo" /></center><br>
-				</div>
       </div>
     </div>
 	</div>
-
-<iframe width=600px height=400px src="https://feed.mikle.com/widget/v2/19732/"></iframe>
-
 <div style="clear: both;">
 </div>
 </div>
-<div id="rss-feeds"></div>
-
+<h1> Football</h1>
+<div id="rss-foot"></div>
+<h1> Tennis</h1>
+<div id="rss-tennis"></div>
+<h1> Rugby</h1>
+<div id="rss-rugby"></div>
+<h1> Basket</h1>
+<div id="rss-basket"></div>
+<h1> Hand</h1>
+<div id="rss-hand"></div>
 
 </body>
 </html>
