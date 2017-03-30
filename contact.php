d5b13b0a9e97b0f1fbc3b70984c618f9063c66e0
Axel Herbert <aherbert97@gmail.com>
2017-03-30 10:15:07 +0200

    modif contact

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
-<?php ini_set('display_errors', true); ?>
+<?php
+ini_set("display_errors",0);error_reporting(0);
+?>
 <html>
 <head>
 	<title>Contact</title>
@@ -28,52 +29,24 @@ include "assets/inc/navbar.php";?>
     <div class="row">
         <div class="col-md-8">
             <div class="well well-sm">
-                <form>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="name">
-                                Nom</label>
-                            <input type="text" class="form-control" id="name" placeholder="Saisir votre nom" required="required" />
-                        </div>
-                        <div class="form-group">
-                            <label for="email">
-                                Adresse email</label>
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
-                                </span>
-                                <input type="email" class="form-control" id="email" placeholder="Saisir votre adresse email" required="required" /></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="subject">
-                                Sujet</label>
-                            <select id="subject" name="subject" class="form-control" required="required">
-                                <option value="na" selected="">Choisir:</option>
-                                <option value="service">Service client</option>
-                                <option value="suggestions">Suggestions</option>
-                                <option value="product">S.A.V</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="name">
-                                Message</label>
-                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
-                                placeholder="Message"></textarea>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <button type="submit" class="btn btn-success pull-right" id="btnContactUs">
-                            Envoyer</button>
-                    </div>
-                </div>
-                </form>
+							<form id="contact" method="post" action="send.php">
+								<fieldset><legend>Vos coordonnées</legend>
+									<p><label for="nom">Nom :</label></br><input type="text" id="nom" name="nom" tabindex="1" class="form-control" /></p>
+									<p><label for="email">Email :</label></br><input type="text" id="email" name="email" tabindex="2" class="form-control" /></p>
+								</fieldset>
+
+								<fieldset><legend>Votre message :</legend>
+									<p><label for="objet">Objet :</label></br><input type="text" id="objet" name="objet" tabindex="3" class="form-control"/></p>
+									<p><label for="message">Message :</label></br><textarea id="message" name="message" tabindex="4" cols="30" rows="8" class="form-control"></textarea></p>
+								</fieldset>
+
+								<div style="text-align:center;"><input type="submit" name="envoi" class="btn btn-success" value="Envoyer" /></div>
+							</form>
             </div>
         </div>
         <div class="col-md-4">
             <form>
-            <legend><span class="glyphicon glyphicon-globe"></span> L'adresse</legend>
+            <legend>L'adresse</legend>
             <address>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2633.404560983434!2d6.211964515430417!3d48.69774791995857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794983386479251%3A0xc58bc09976474b86!2s13+Rue+Jean+Moulin%2C+54510+Tomblaine!5e0!3m2!1sfr!2sfr!4v1488560215757" width="350" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>            </address>
             <address>
