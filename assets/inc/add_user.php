2efcef9e43f5b3a0abddee9b966d9e8462241fb9
Antoine Jean <antoinejeanmec@hotmail.fr>
2017-04-23 20:20:59 +0200

    maj inscriptions (fonctionnelles)
    dzefjozeirng

--- assets/inc/add_user.php
+++ assets/inc/add_user.php
@@ -4,15 +4,26 @@ include 'connect.php';
 
 $bdd = connectToSQL();
 
-print_r($_POST);
+$type = "basic_user";
 
-$req = $bdd->prepare('INSERT INTO users(pseudo,mail,password,type,prenom,nom) VALUES(?,?,?,?,?,?,?)');
+/*$req = $bdd->prepare('INSERT INTO users(pseudo,mail,password,type,prenom,nom) VALUES(?,?,?,?,?,?)');
 $req->execute(array($_POST['pseudo'],
                     $_POST['mail'],
                     $_POST['password'],
                     'basic_user',
                     $_POST['prenom'],
-                    $_POST['nom']));
+                    $_POST['nom']));*/
+
+$req = $bdd->prepare('INSERT INTO users(pseudo,mail,password,type,prenom,nom)
+                      VALUES(:pseudo, :mail, :password, :type, :prenom, :nom)');
+$req->execute(array(
+                    	'pseudo' => $_POST['pseudo'],
+                    	'mail' => $_POST['mail'],
+                    	'password' => $_POST['password'],
+                    	'type' => $type,
+                    	'prenom' => $_POST['prenom'],
+                    	'nom' => $_POST['nom']
+                    	));
 
 header('location: ../../index.php');
 
