cc5206dd2c6acbbe807a74b0a2d542e68308e8a5
Antoine Jean <antoinejeanmec@hotmail.fr>
2017-04-27 10:27:26 +0200

    réorganisation du code de news.php

--- news.php
+++ news.php
@@ -9,51 +9,22 @@ session_start();
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/rss.js"></script>
-
-    <script>
-      jQuery(function($) {
-        $("#rss-foot").rss("http://www.lequipe.fr/rss/actu_rss_Football.xml")
-      })
-    </script>
-      <script>
-      jQuery(function($) {
-        $("#rss-tennis").rss("http://www.lequipe.fr/rss/actu_rss_Tennis.xml")
-      })
-    </script>
-     <script>
-      jQuery(function($) {
-        $("#rss-rugby").rss("http://www.lequipe.fr/rss/actu_rss_Rugby.xml")
-      })
-    </script>
-    <script>
-      jQuery(function($) {
-        $("#rss-basket").rss("http://www.lequipe.fr/rss/actu_rss_Basket.xml")
-      })
-    </script>
-    <script>
-      jQuery(function($) {
-        $("#rss-hand").rss("http://www.lequipe.fr/rss/actu_rss_Hand.xml")
-      })
-    </script>
+<script src="assets/js/rss.js"></script>
 
 </head>
 <body>
 		<?php include "assets/inc/navbar.php"; ?>
 
-	<div class="jumbotron jumbotron-sm">
-    <div class="container">
-			<center>
-				<div class="row">
+		<div class="jumbotron jumbotron-sm">
+			<div class="container">
+				<center>
+					<div class="row">
 						<h2 class="h2">News</h2>
-				</div>
-			</center>
-      </div>
-    </div>
-	</div>
-<div style="clear: both;">
-</div>
-</div>
+					</div>
+				</center>
+			</div>
+		</div>
+
 <h1> Football</h1>
 <div id="rss-foot"></div>
 <h1> Tennis</h1>
@@ -69,4 +40,31 @@ session_start();
 <?php include "assets/inc/footer.php" ?>
 </footer>
 </body>
+
+<script>
+	jQuery(function($) {
+		$("#rss-foot").rss("http://www.lequipe.fr/rss/actu_rss_Football.xml")
+	})
+</script>
+	<script>
+	jQuery(function($) {
+		$("#rss-tennis").rss("http://www.lequipe.fr/rss/actu_rss_Tennis.xml")
+	})
+</script>
+ <script>
+	jQuery(function($) {
+		$("#rss-rugby").rss("http://www.lequipe.fr/rss/actu_rss_Rugby.xml")
+	})
+</script>
+<script>
+	jQuery(function($) {
+		$("#rss-basket").rss("http://www.lequipe.fr/rss/actu_rss_Basket.xml")
+	})
+</script>
+<script>
+	jQuery(function($) {
+		$("#rss-hand").rss("http://www.lequipe.fr/rss/actu_rss_Hand.xml")
+	})
+</script>
+
 </html>
